5e4e570bf187e861885962afd11669643387ecb6
douglasklemes0303@gmail.com <douglasklemes0303@gmail.com>
2019-08-01 14:17:56 -0300

    Adicionar js

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 let nome = document.querySelector("#nome");
 let email = document.querySelector("#email");
-let confirmaçaoDeEmail = document.querySelector("#confirmaçao de email");
+let confirm = document.querySelector("#confirm");
 let senha = document.querySelector("#senha");
-let botao = document.querySelector("#botao");
+let botao = document.querySelector("button");
 let sexo = document.querySelector("#sexo");
 
 function cadastro() {
-    if(email.value === confirmaçaoDeEmail){
-        localStorage.setItem("sexo" ,sexo.value);
+    console.log("a")
+    if(email.value === confirm.value){
         localStorage.setItem("nome" ,nome.value);
         localStorage.setItem("email" ,email.value);
         localStorage.setItem("senha" ,senha.value);
-
+        localStorage.setItem("sexo" ,sexo.value);
+    }
+    else{
+   
     }
 }
 
